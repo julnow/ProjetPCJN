1ccf5e3b5188a35a877dfa731bd1c87211758182
julnow <jul.nowk@gmail.com>
2019-12-06 15:00:50 +0100

    testowyprogram

--- src/Program.java
+++ src/Program.java
@@ -7,7 +7,14 @@ public class Program {
 		Member john = new ClassNormal("john");
 		Member pablo = new ClassZero("pablo");
 		Member katia = new ClassDemie("katia");
-		admin.addMember(network1, m);
+		admin.addMember(network1, katia);
+		admin.addMember(network1, pablo);
+		admin.addMember(network1, john);
+		
+		Service netoyage = new Service("netoyage", 15);
+		Service jardinage = new Service("jardinage", 15);
+		Service netoyage = new Service("netoyage", 15);
+		Service netoyage = new Service("netoyage", 15);
 
 	}
 
