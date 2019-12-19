e046b22e529710ebc5fe019204cf6a972e17deaf
julnow <jul.nowk@gmail.com>
2019-12-19 17:42:09 +0100

    funkcja pokazujaca ludzi i pieniadze ver2

--- src/Network.java
+++ src/Network.java
@@ -27,7 +27,7 @@ public class Network {
 		else{
 			boolean found = false;
 			while(!found) {
-				for(int i = 0; i < members.size(); ++i) {
+				for(int i = 0; i < members.size()-1; ++i) {
 					if (members.get(i).getName() == name) {
 						members.remove(i);
 						found = true;	
@@ -63,13 +63,5 @@ public class Network {
 			}
 		}
 		return can;
-	}
-	String usersMoney() {
-		String tmp = "";
-		for(int i = 0; i<this.members.size(); ++i) {
-			tmp += members.get(i).getName() + ": " + members.get(i).getJetons() +" ";
-		}
-		return tmp;
-		
 	}
 }
--- src/Program.java
+++ src/Program.java
@@ -3,78 +3,48 @@ public class Program {
 	
 
 	public static void main(String[] args) {
-	
+		try {
 		Admin admin = new Admin("admin", "admin123");
-		Member john = new ClasseNormal("john");
-		Member pablo = new ClasseZero("pablo");
-		Member katia = new ClasseDemie("katia");
-		Network network1 = admin.createNetwork();
-		
-		try { //adding members to network
+			Network network1 = admin.createNetwork();
+			Member john = new ClasseNormal("john");
+			Member pablo = new ClasseZero("pablo");
+			Member katia = new ClasseDemie("katia");
 			admin.addMember(network1, katia);
 			admin.addMember(network1, pablo);
 			admin.addMember(network1, john);
-		}
-		catch (Exception e1) {
-			System.out.println(e1);
-		}
-		//example of trying to add a member without the admin
-		Member nicolas = new ClasseNormal("nicolas");
-		try { 
-			network1.addMember(nicolas, "qwerty"); //false password
-		}
-		catch (Exception e2) {		
-			System.out.println(e2);
-		}
-		try { 
-			admin.addMember(network1, nicolas);
-		}
-		catch (Exception e3) {		
-			System.out.println(e3);
-		}
-		
-		Service netoyage = new Service("netoyage", 15);
-		Service jardinage = new Service("jardinage", 10);
-		Service lavage = new Service("netoyage", 10);
-		Service repair = new Service("repair", 5);
-		
-		try {
-			//on va donner le meme comptences aux Pablo et Nicolas pour mieux presenter selection aleatoire de travaillers
+			
+			Member nicolas = new ClasseNormal("nicolas");
+			network1.addMember(nicolas, "admin123");
+			
+			Service netoyage = new Service("netoyage", 15);
+			Service jardinage = new Service("jardinage", 10);
+			Service lavage = new Service("netoyage", 10);
+			Service repair = new Service("repair", 5);
+			
 			pablo.addCompetence(netoyage);
 			pablo.addCompetence(jardinage);
-			nicolas.addCompetence(netoyage);
-			nicolas.addCompetence(jardinage);
-			
 			katia.addCompetence(repair);
 			katia.addCompetence(jardinage);
 			john.addCompetence(lavage);
 			john.addCompetence(netoyage);
-		}
-		catch (Exception e4) {
-			System.out.println(e4);
-		}	
-		
-		System.out.println(network1.usersMoney());	
-		try{	
+			
+			
+			
+			System.out.println("John: " + john.getJetons() + "Katia: "+ katia.getJetons() +  "Pablo: " + pablo.getJetons());
 			
 			Task task1 = john.commandTask(jardinage, 2, 1);
+			System.out.println(task1.getNbPerson());
+			System.out.println(network1.nbCompetence(task1.getService()));
+			
 			admin.validateTask(task1, network1);
 			
-			System.out.println(network1.usersMoney());
+			System.out.println("John: " + john.getJetons() + "Katia: "+ katia.getJetons() +  "Pablo: " + pablo.getJetons());
 		}
-		catch (Exception e5) {
-			System.out.println(e5);
+		catch (Exception e) {
+	
+			System.out.println(e);
 		}
-		//removing member
-		try{	
-			admin.removeMember(network1, nicolas);
-			
-			System.out.println(network1.usersMoney());
-		}
-		catch (Exception e6) {
-			System.out.println(e6);
-		}
-		
-	}
-}		
 
+	}
+
+}
